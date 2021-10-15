41c3c1a2589244957fce540fb381fe73fbcb26c2
No4ovka <92082996+No4ovka@users.noreply.github.com>
2021-10-15 09:35:38 +0300

    Add files via upload

--- rev_null_num.cpp
+++ rev_null_num.cpp
@@ -1,12 +1,12 @@
 #include "middle.h"
 
 int itc_rev_num(long long num1){
-    int chis=0,num=num1;
+    long long chis=0,num=num1;
     while (num1!=0){
         chis=chis*10+num1%10;
         num1/=10;
-    }
-    chis=itc_len_num(chis);
+    }chis=chis/10;
+    chis=dlinna(chis);
     return chis;
 }
 int itc_null_count(long long num1){
--- second_max.cpp
+++ second_max.cpp
@@ -1,16 +1,19 @@
 #include "middle.h"
-
 int itc_second_max_num(long long num1){
-    int max2=0,maxim=0,num=num1,num2=0;
-    if (num1<0)
-        num*=(-1);
+    num1=modyl(num1);
+    long long max2=0,maxim=0,num=num1,num2=0,imax=0, index=0, ind=0,num5=perevorot(num1);
     while (num!=0){
-        if (num%10>maxim)
+        if (num%10>maxim){
             maxim=num%10;
-        else if (num1%10<=maxim)
-            num2=num2*10+num%10;
-        num/=10;
-    }
+            imax=index;}
+        index++;
+        num/=10;}
+    imax=index-imax;
+    while (ind<index+1){
+        ind ++;
+        if (ind!=imax)
+            num2=num2*10 + num5%10;
+        num5/=10;}
     while (num2!=0){
         if (num2%10<=maxim && num2%10>max2)
             max2=num2%10;
